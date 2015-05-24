e262f451b2436637fba00e757a485d5b0b5bc0eb
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-05-24 00:44:28 -0700

    Added new plots

--- Wiki.R
+++ Wiki.R
@@ -1,24 +1,26 @@
-setwd('/Users/mmartinez/Desktop/search_task/')
+setwd('/Users/mmartinez/Desktop/Wikimedia Analysis//')
 library("ggplot2")
 library(ggplot2)
 options(scipen=999)
 
-data <- read.table("search_dataset.tsv",sep="\t", header=TRUE)
+data <- read.table("search_dataset.tsv", sep="\t", header=TRUE)
 
 ###Convert timestamp to date
 data$timestamp <- strptime(data$timestamp,"%Y%m%d%H%M%S")
 
 ###Extract weekday and hour from date to create two new columns
 data$Weekday <- weekdays(data$timestamp)
+data$Weekday <- factor(data$Weekday, levels= c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"))
 data$Hour <- substr(data$timestamp , 12,13)
 data$TimeOfDay <- cut(as.numeric(data$Hour), 
                       breaks=c(0,6,12,18,24), 
                       labels=c("Early Morning","Morning","Afternoon","Evening"),
                       include.lowest=T)
-
+head(data)
 ###Average data by Weekday + Hour for d3 visual
+results <- data[data$event_action == "results", ]
+head(results)
 averagedatedata <- aggregate(event_timeToDisplayResults ~ Weekday + Hour, data = results, FUN= "median" )
-averagedatedata$Weekday <-factor(averagedatedata$Weekday, levels= c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"))
 
 ##Heat map 
 p <- ggplot(averagedatedata,aes(x = Hour, y = Weekday, fill = event_timeToDisplayResults )) 
@@ -26,10 +28,10 @@ p <- p + geom_tile(aes(fill=event_timeToDisplayResults), colour="white")
 p <- p + scale_fill_gradient(low = "white",high = "steelblue")
 p
 
+weekday_freq <- data.frame(table(data$Weekday))
+weekday_freq <- weekday_freq[sort(weekday_freq$Var1), ]
 
-
-
-
-
-
-
+##Bar plot of freq of different weekdays
+ggplot(weekday_freq, aes(factor(Var1), y = as.numeric(Freq))) + 
+  geom_bar(colour = "black", fill = "#DD8888", width = .8, stat = "identity") + 
+  coord_flip()
