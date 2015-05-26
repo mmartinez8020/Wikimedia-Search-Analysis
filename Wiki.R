c4230eef5bcc473c11619e23a8a0a021e31a8cdf
Mark Martinez <mmartinez@Marks-MacBook-Pro.local>
2015-05-25 17:31:10 -0700

    Done with analysis

--- Wiki.R
+++ Wiki.R
@@ -9,6 +9,8 @@ require(knitr) # required for knitting from rmd to md
 require(markdown) # required for md to html 
 options(scipen=999)
 
+
+
 data <- read.table("search_dataset.tsv", sep="\t", header=TRUE)
 summary(data)
 ###Convert timestamp to date
@@ -19,19 +21,9 @@ data$Weekday <- weekdays(data$timestamp)
 data$Weekday <- factor(data$Weekday, levels= c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"))
 
 data$Hour <- substr(data$timestamp , 12,13)
-
-#data$TimeOfDay <- cut(as.numeric(data$Hour), 
-#                      breaks=c(0,6,12,18,24), 
-#                      labels=c("Early Morning","Morning","Afternoon","Evening"),
-#                      include.lowest=T)
-
 data$Month <- months(data$timestamp)
 data$Month <- factor(data$Month, levels= c("November","December","January","February","March","April","May"))
-
-
-
-
-
+head(data)
 
 ##Bar plots
 
@@ -44,12 +36,14 @@ plottingfunction <- function(data, xlabel){
     geom_bar(fill="lightblue", colour="black",stat = "identity") +
     xlab(xlabel) +
     ylab("Frequency") +
-    coord_flip()
+    coord_flip() +
+    theme(axis.text=element_text(size=20),
+          axis.title = element_text(size=40,face="bold"),
+          plot.title = element_text(size =60, face="bold",lineheight=.8))
   
   return(plot)
 }
-table(data$event_action)
-693723/163943
+
 eventactionbar <- plottingfunction(data$event_action,"Event Action")
 weekdaybar <- plottingfunction(data$Weekday,"Weekday")
 monthbar <- plottingfunction(data$Month,"Month")
@@ -58,7 +52,8 @@ grid.arrange(eventactionbar, weekdaybar, monthbar, ncol=3)
 
 #####Histogram by Month
 
-resulthist <- data[data$event_action == "results" & (data$event_timeToDisplayResults < 2000), ] #Cut off long tail
+resulthist <- data[data$event_action == "results" & !(data$event_timeToDisplayResults < 2000), ] #Cut off long tail
+nrow(resulthist)/nrow(data)
 resulthist <- resulthist[resulthist$event_timeToDisplayResults > 0, ]#Remove values less than 0
 
 resulthistogram <- ggplot(resulthist, aes(x=event_timeToDisplayResults, y = ..density..)) + 
@@ -91,7 +86,7 @@ eventheat
 
 freqofactions <- data[ ,c(4,5)] %>% group_by(Weekday,Hour)  %>%
   summarize(Count = n())
-freqofactions
+head(freqofactions)
 heataction <- ggplot(freqofactions,aes(x = Hour, y = Weekday, fill = Count )) +
                       geom_tile(aes(fill=Count), colour="white") +
                       scale_fill_gradient(low = "white",high = "steelblue") +
@@ -100,9 +95,51 @@ heataction <- ggplot(freqofactions,aes(x = Hour, y = Weekday, fill = Count )) +
                             axis.title=element_text(size=40,face="bold"),
                             legend.text=element_text(size=20),
                             legend.title=element_text(size=20)) 
-                      
 
+heataction
+################################
+#########Time series############
+################################
+
+##Data preparation
+datawithoutmonths <- data[!(data$Month == "November" | data$Month == "May"), ] ##Remove November and May dates from df
+datawithoutmonths$date  <- as.Date(datawithoutmonths$timestamp)
+
+datawithoutmonths$week <- cut(datawithoutmonths[,"date"],breaks = 'weeks')
+df <- data.frame(table(datawithoutmonths$week,datawithoutmonths$event_action))
+df$Var1 <- as.Date(df$Var1)
+
+##Plotting
+timeclicks <- ggplot(df[df$Var2 == "click",],aes(x=Var1,y=Freq)) + 
+                                             xlab("Clicks") +
+                                             geom_line() +
+                                             theme(axis.text=element_text(size=20),
+                                                  axis.title=element_text(size=40,face="bold"),
+                                                  legend.text=element_text(size=20),
+                                                  legend.title=element_text(size=20)) 
+
+head(data[,c(4,6,7)])
+#time series for clicks
+timeresults <- ggplot(df[df$Var2 == "results",],aes(x=Var1,y=Freq)) + 
+                                            xlab("Results") +
+                                            geom_line() +
+                                            theme(axis.text=element_text(size=20),
+                                                  axis.title=element_text(size=40,face="bold"),
+                                                  legend.text=element_text(size=20),
+                                                  legend.title=element_text(size=20)) 
+
+timestarts <- ggplot(df[df$Var2 == "start",],aes(x=Var1,y=Freq)) +
+                                            xlab("Starts") +
+                                            geom_line() +
+                                            theme(axis.text=element_text(size=20),
+                                                  axis.title=element_text(size=40,face="bold"),
+                                                  legend.text=element_text(size=20),
+                                                  legend.title=element_text(size=20)) 
+
+grid.arrange(timeclicks,timestarts,timeresults,main = textGrob("Event Action by Week",gp=gpar(fontsize=60)))
 require(knitr) # required for knitting from rmd to md
 require(markdown) # required for md to html 
 knit('markup.rmd', 'docs.md')
-markdownToHTML('docs.md', 'test.html')
+markdownToHTML('docs.md', 'WikimediaAnalysis.html',header = TRUE)
+
+
---
+++ docs1.md
@@ -0,0 +1,11 @@
+---
+title: "Untitled"
+author: "Mark Martinez"
+date: "May 25, 2015"
+output: html_document
+\VignetteEngine{knitr::docco_linear}
+---
+
+```r
+x <- 2+ 2
+```
